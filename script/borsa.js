8d0f62bb12147eba65c6fec5f588907f54412b2d
gmarty2000 <43878483+gmarty2000@users.noreply.github.com>
2018-12-03 17:31:35 +0100

    Aggiunta e modifica dei vari commenti

--- script/borsa.js
+++ script/borsa.js
@@ -1,20 +1,22 @@
-/* TODO: intestazione */
+/* Costanti - 	1. 	Il numero dei tipi di piastrelle
+ * 		  	  2. 	Il numero di piastrelle per tipo
+ * 		  	  3. 	Il numero di piastrelle per espositore
+ */
 const N_TIPI_PIASTRELLE = 5;
 const N_PIASTRELLE_PER_TIPO = 20;
 const N_PIASTRELLE_PER_ESPOSITORE = 4;
 
+/* Array - Il contenitore per le piastrelle */
 var piastrelle = new Array();
 
-/* TODO: descrizione */
-function caricaBorsa() {
-	for (var nTipiPiastrelle = 0; nTipiPiastrelle < N_TIPI_PIASTRELLE; nTipiPiastrelle++) {
-		for (var nPiastrelle = 0; nPiastrelle < N_PIASTRELLE_PER_TIPO; nPiastrelle++) {
+/* Funzione (Evento) - Carica la borsa con 100 piastrelle (20 per tipologia) */
+function eventoCaricaBorsa() {
+	for (var nTipiPiastrelle = 0; nTipiPiastrelle < N_TIPI_PIASTRELLE; nTipiPiastrelle++)
+		for (var nPiastrelle = 0; nPiastrelle < N_PIASTRELLE_PER_TIPO; nPiastrelle++)
 			piastrelle.push(nTipiPiastrelle);
-		}
-	}
 }
 
-/* TODO: descrizione */
+/* Funzione - Estrae casualmente dalla borsa 4 piastrelle */
 function prendiPiastrelle() {
 	var piastrellePrese = null;
 	
@@ -27,9 +29,10 @@ function prendiPiastrelle() {
 		}
 		
 	} else
-		errore(ERRORE_2, -2);
+		errore('Tessere esaurite', -2);
 	
 	return piastrellePrese;
 }
 
-window.addEventListener(EVENTO_LOAD, caricaBorsa);
+/* Listener - Quando la finestra (window) viene caricata (EVENTO_LOAD), viene invocata la funzione 'eventoCaricaBorsa()' */
+window.addEventListener(EVENTO_LOAD, eventoCaricaBorsa);
